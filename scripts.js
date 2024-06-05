c23c4f59cbdf3eab05ab357ccaff02fcc1b5c89e
Joel <joeldavidjacob@outlook.com>
2024-06-05 20:07:19 +0400

    QOL changes, chip design

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-var deck = [];
-for (i = 1; i <= 10; i++) {
-  for (j = 1; j <= 4; j++) {
-    deck.push(i);
-  }
-}
-for (k = 0; k < 12; k++) {
-  deck.push(10);
-}
-// created deck with 52 cards with their values
-
-let dealer = [];
-let player = []; // empty array containing dealers cards
+var deck = generateDeck();
+console.log(deck);
 let playersCount = 0;
 let dealersCount = 0;
 
@@ -19,49 +8,28 @@ let playerAlive = true;
 let dealerArea = document.querySelector('.dcards').innerHTML;
 let playerArea = document.querySelector('.pcards').innerHTML;// init html of cards to be rendered
 
-
-document.querySelector('.js-play').addEventListener("click", playGame);
-
-function endGame() {
-  document.querySelector('.dcards').innerHTML = '';
-  document.querySelector('.pcards').innerHTML = '';
-  document.querySelector('.js-play').addEventListener("click", playGame);
-  playerAlive = false;
-  document.querySelector('.js-hit').removeEventListener("click", hitHandler);
-  document.querySelector('.js-stand').removeEventListener("click", standHandler);
-  playersCount = 0;
-  dealersCount = 0;
-  document.getElementById('dealer-count').innerHTML = `Dealer: ${dealersCount}`;
-  document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
-}
-
-function renderCard(num)
-{
-  return `<div class = card>${num}</div>`;
-}
+document.querySelector('.js-play').addEventListener("click", playGame);//adds eventlistener to deal button
 
 function playGame() {
   document.querySelector('.js-play').removeEventListener("click", playGame);
   document.getElementById('mid-text').textContent = '';
-  setTimeout(() => {
+  setTimeout(() => {//deals 1st dealer card
     dealerscard = randomCard(); // pulls random card
     document.querySelector('.dcards').innerHTML += renderCard(dealerscard);
     dealersCount += dealerscard;
     document.getElementById('dealer-count').innerHTML = `Dealer: ${dealersCount}`;
   }, 800);
 
-  setTimeout(() => {
+  setTimeout(() => {//deals 1st player card
     playerscard = randomCard();
-    player.push(playerscard);
     document.querySelector('.pcards').innerHTML += renderCard(playerscard);
     playersCount += playerscard;
     document.getElementById('player-count').innerHTML = `Player: ${playerscard}`;
   }, 1600);
 
-  // Deal one more card to the player
-  setTimeout(() => {
+  
+  setTimeout(() => {// Deal one more card to the player
     let additionalCard = randomCard();
-    player.push(additionalCard);
     document.querySelector('.pcards').innerHTML += renderCard(additionalCard);
     playersCount += additionalCard;
     document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
@@ -71,86 +39,111 @@ function playGame() {
   playerGame(playersCount);
 }
 
-function playerGame(playersCount) {
+function playerGame(playersCount) {//adds event listeners for hit and stand
   if (playersCount <= 21) {
     document.querySelector('.js-hit').addEventListener("click", hitHandler);
     document.querySelector('.js-stand').addEventListener("click", standHandler);
   } else {
-    console.log('count: busted');
-    setTimeout(endGame,400);
-    return playersCount;
+    setTimeout(endGame,2000);
   }
-  return null;
 }
 
-function hitHandler() {
+function hitHandler() {//instructions for when hit is pressed
   let pcard = randomCard();
-  console.log('New card: ' + pcard);
   document.querySelector('.pcards').innerHTML  += renderCard(pcard);
   playersCount += pcard;
   document.getElementById('player-count').innerHTML = `Player: ${playerscard}`;
-
+  //generates new card
   if (playersCount <= 21) {
-    console.log('Players count: ' + playersCount);
     document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
-    // Update the player display if needed
-  } else {
-    console.log('count: busted');
+    // Update the player display
+  } else {//player busts
     document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
     document.getElementById('mid-text').textContent = 'Player Busts. You lose.';
-    setTimeout(endGame,400);
-    //window.location.reload();
+    setTimeout(endGame,2000);
   }
 }
 
-function standHandler() {
+function standHandler() {//when stand is clicked
   document.getElementById('mid-text').textContent = '';
   document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
   document.getElementById('mid-text').textContent = `Player: Stands on ${playersCount}`;
   setTimeout(()=>{dealersGame(playersCount);},500);
 }
 
-
-
 async function dealersGame(playersCount) {
   // Helper function to add delay
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  while (dealersCount < 17) {
+  while (dealersCount < 17) {//keeps dealing cards until 17 or higher
     let dcard = randomCard();
-    console.log('New card for Dealer: ' + dcard);
     document.querySelector('.dcards').innerHTML += renderCard(dcard);
     dealersCount += dcard;
     document.getElementById('dealer-count').innerHTML = `Dealer: ${dealersCount}`;
-    console.log("Dealer count: " + dealersCount);
     await delay(1000); // Wait for 300ms before drawing the next card
   }
-  if (dealersCount > 21) {
+  if (dealersCount > 21) {//checks for bust
     document.getElementById('dealer-count').innerHTML = `Dealer: ${dealersCount}`;
     document.getElementById('mid-text').textContent = `Dealer Busts. You Win!`;
-    setTimeout(endGame,400);
-    //window.location.reload();
+    setTimeout(endGame,2000);
   } else {
-    if ((21 - playersCount) === (21 - dealersCount)) {
+    if ((21 - playersCount) === (21 - dealersCount)) {//checks for push
       document.getElementById('mid-text').textContent = `Push.`;
-      setTimeout(endGame,400);
-      //window.location.reload();
-    } else if ((21 - playersCount) > (21 - dealersCount)) {
-      document.getElementById('mid-text').textContent = `You Win!`;
-      setTimeout(endGame,400);
-      //window.location.reload();
-    } else {
-      document.getElementById('mid-text').textContent = `You Lose.`;
-      setTimeout(endGame,400);
-      //window.location.reload();
+      setTimeout(endGame,2000);
+
+    } else if ((21 - playersCount) > (21 - dealersCount)) {//checks for player win
+      document.getElementById('mid-text').textContent = `You Lose!`;
+      setTimeout(endGame,2000);
+
+    } else {//checks for player win
+      document.getElementById('mid-text').textContent = `You Win.`;
+      setTimeout(endGame,2000);
     }
   }
 }
 
 
-function randomCard() {
+function randomCard()//Generates random card from deck
+{
   let cardIndex = Math.floor(Math.random() * deck.length); // Get a random index within the deck length
   let removedCard = deck.splice(cardIndex, 1)[0]; // Remove the card from the deck and get the removed card
   return removedCard; // Return the removed card
+}
+
+function endGame() {//instructions for ending game and resetting so that deal can be clicked again
+  document.querySelector('.dcards').innerHTML = '';
+  document.querySelector('.pcards').innerHTML = '';
+  //resetting cards on screen
+  document.querySelector('.js-play').addEventListener("click", playGame);
+  //adds event listener back to program
+  playerAlive = false;
+  document.querySelector('.js-hit').removeEventListener("click", hitHandler);
+  document.querySelector('.js-stand').removeEventListener("click", standHandler);
+  //removes event listeners from hit and stand
+  playersCount = 0;
+  dealersCount = 0;
+  document.getElementById('dealer-count').innerHTML = `Dealer: ${dealersCount}`;
+  document.getElementById('player-count').innerHTML = `Player: ${playersCount}`;
+  //resets counters
+  deck = generateDeck();//makes new deck
+}
+
+function renderCard(num)//generates html of card
+{
+  return `<div class = card>${num}</div>`;
+}
+
+function generateDeck()// created deck with 52 cards with their values
+{
+  let deck = [];
+  for (i = 1; i <= 10; i++) {
+    for (j = 1; j <= 4; j++) {
+      deck.push(i);
+    }
+  }
+  for (k = 0; k < 12; k++) {
+    deck.push(10);
+  }
+  return deck;
 }
